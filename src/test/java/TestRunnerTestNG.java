d882bc28663430a0a89463f0b0e9490007509e18
Monga <amonga@deloitte.com>
2020-03-09 15:13:43 +0530

    TestRunnerTestNG

--- src/test/java/TestRunnerTestNG.java
+++ src/test/java/TestRunnerTestNG.java
@@ -3,6 +3,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(features = {"src/test/java/FeatureFile"},
         plugin = {"json:target/cucumber.json", "html:target/site/cucumber-pretty"},
-        glue = "StepDefinition")
+        glue = "StepDefinition"
+)
 public class TestRunnerTestNG extends AbstractTestNGCucumberTests {
 }
